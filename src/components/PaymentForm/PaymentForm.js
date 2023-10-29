bccd28457a8f5d06ef4d2c38fb97a28b9ce24c37
Rina Dz <dz.rina.na@gmail.com>
2023-10-29 23:09:49 +0300

    dev: сверстать страницу с формой

--- src/components/PaymentForm/PaymentForm.js
+++ src/components/PaymentForm/PaymentForm.js
@@ -2,33 +2,50 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 //import useFormWithValidation from '../../hooks/useFormValidator';
 import "./PaymentForm.css";
+import background_img from '../../images/background.png';
 
 function PaymentForm(props) {
 
-    // const { values, handleChange, handleSubmit, errors, isValid } = useFormWithValidation(props.handleSubmit);
-    // <div className="payment-form__container">
-    //                 <h2 className="payment-form__title">Редактировать профиль</h2>
-    //                 <form className="payment-form__form" onSubmit={handleSubmit}>
-    //                     <label className="payment-form__label">Имя</label>
-    //                     <input className="payment-form__input" placeholder={props.userName} name="name"
-    //                         value={values?.name} onChange={handleChange} minLength={2} required />
-    //                     <span className="payment-form__input-error">{errors?.name}</span>
-    //                     <label className="payment-form__label">E-mail</label>
-    //                     <input className="payment-form__input" placeholder={props.userEmail} type='email'
-    //                         pattern='^[a-z0-9A-Z._%+-]+@[a-z0-9A-Z.-]+\.[a-zA-Z]{2,}$'
-    //                         name="email" value={values?.email} onChange={handleChange} minLength={2} required />
-    //                     <span className="payment-form__input-error">{errors?.email}</span>
-    //                     <button className={isValid ? `payment-form__button payment-form__button-enable button` : 'profile-form__button profile-form__button-disable'} type='submit'>Редактировать</button>
-    //                 </form>
-    //                 <div className="payment-form__links">
-    //                     <Link className="payment-form__link button" to="/profile">Вернуться к профилю</Link>
-    //                 </div>
-    //             </div>
+
 
     return (
         <>
             <section className="payment-form">
-                
+                <div className="payment-form__container">
+                    <h2 className="payment-form__title">Сертификат на 50000 руб</h2>
+                    <form className="payment-form__form">
+
+                        <label className="payment-form__label">ФИО *</label>
+                        <input className="payment-form__input" name="name" minLength={2} required placeholder='Введите...' />
+                        <span className="payment-form__input-error"></span>
+
+                        <label className="payment-form__label">Телефон *</label>
+                        <input className="payment-form__input" type='tel' inputmode="numeric"
+                            name="tel" minLength={11} required placeholder="+7 (000) 000-00-00"
+                        />
+                        <span className="payment-form__input-error"></span>
+
+                        <label className="payment-form__label">Сообщение</label>
+                        <textarea className="payment-form__input text-input" name="message" maxLength={320} cols="40"
+                            rows="3" placeholder='Введите...' />
+                        <span className="payment-form__input-error"></span>
+
+                        <label className="payment-form__label">E-mail *</label>
+                        <input className="payment-form__input" type='email' inputmode="email"
+                            pattern='^[a-z0-9A-Z._%+-]+@[a-z0-9A-Z.-]+\.[a-zA-Z]{2,}$'
+                            name="email" minLength={2} required placeholder='Введите...' />
+                        <span className="payment-form__input-error"></span>
+
+                    </form>
+                    <Link className="payment-form__rules-link button" to="https://sycret.ru/">Правила</Link>
+
+
+                    <div className="payment-form__links">
+                        <Link className="payment-form__link button" to="/">Назад</Link>
+                        <Link className="payment-form__link button" to="/payment">Перейти к оплате</Link>
+                    </div>
+                </div>
+                <img className="background-img" src={background_img} alt="Фоновое изображение" />
             </section>
         </>
     )
